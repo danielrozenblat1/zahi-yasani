1f9cd182a478213f45c7475136adfe954cf38140
Your Name <you@example.com>
2025-01-03 16:32:32 +0200

    newChnages

--- src/components/recommends/Services.js
+++ src/components/recommends/Services.js
@@ -6,6 +6,7 @@ import result1 from "../../images/צהיי שירותים 1.png";
 import result2 from "../../images/צהיי שירותים 2.png";
 import result3 from "../../images/צהיי שירותים 3.png";
 import result4 from "../../images/צהיי שירותים 4.png";
+import result10 from "../../images/צהיי נאום.png";
 import result5 from "../../images/צהיי שירותים 5.png";
 import result6 from "../../images/צהיי שירותים 6.png";
 import result7 from "../../images/צהיי שירותים 7.png";
@@ -13,7 +14,7 @@ import result8 from "../../images/צהיי שירותים 8.png";
 import result9 from "../../images/צהיי ערב נשים.jpeg";
 const Services = () => {
   const images = [
-    result1, result2, result3, result4,
+    result1, result2, result3, result4,result10,
     result5, result6, result7, result8,result9
   ];
 
